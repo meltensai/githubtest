64424fa3e70643cccb886cbacc19d5c9f87528c8
meltensai <melthetensai@gmail.com>
2023-04-25 17:35:51 +0900

    2nd commit

--- src/main/java/com/project/member2/board/Controller/b_MainController.java
+++ src/main/java/com/project/member2/board/Controller/b_MainController.java
@@ -1,14 +1,38 @@
 package com.project.member2.board.Controller;
 
+import com.project.member2.board.dto.BoardDTO;
+import com.project.member2.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board") //모든 getmapping에 /board 가 기본으로 깔림
 public class b_MainController {
+    private final BoardService boardService;
+
     @GetMapping("/write")
     public String writeForm() {
         return "board_write";
+    }
+
+    @PostMapping("/write") //RequestParam을 통해 각각 값의 name을 괄호 안에 넣고, String 값으로 받기
+    public String write(@ModelAttribute BoardDTO boardDTO) {
+        boardService.save(boardDTO);
+        return "board_index";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        //DTO를 전부 다 list 형태로 받음
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        //받은 list값을 model로 변환("boardList"라는 이름으로..)
+        model.addAttribute("boardList", boardDTOList);
+        //"board_list" html에서 보여줌
+        return "board_list";
     }
 }
